52f127b30f2280385a8af8586030096055778f36
henriquedias420 <henriquediasper@hotmail.com>
2023-06-27 14:56:05 -0300

    Adicionei o typescript no package-json - adicionei a tipagem do node (@types/node) - refatorei o package-json retirando a linha type - criei a pasta src - adicionei o arquivo index.ts a pasta src - editei o script start para executar a transpilacao e rodar o javascript da pasta build - criei a pasta types e criei o codigo das tipagens para users e products e exportei - criei a pastas database.ts e importei as constantes TUsers e TProducts tipando-as com um array de objetos - verifiquei se a aplicacao esta funcionando importando as constantes users e products no arquivo index.ts e dando um console.log nas constantes

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+const database_2 = require("./database");
+console.log('Usuários:', database_1.users);
+console.log('Produtos:', database_2.products);
+//# sourceMappingURL=index.js.map
