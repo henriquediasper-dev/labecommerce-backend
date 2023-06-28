f15a67c301141086846de104c7ec1c2d3eeeb027
henriquedias420 <henriquediasper@hotmail.com>
2023-06-28 15:40:26 -0300

    [IMPLEMENTACAO] desenvolvi as funcoes de createUser, getAllUsers, createProduct, getAllProducts e searchProductsByName para cada funcionalidade

--- build/index.js
+++ build/index.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
-const database_2 = require("./database");
-console.log('Usuários:', database_1.users);
-console.log('Produtos:', database_2.products);
+(0, database_1.createTUser)('u003', 'Ciclana', 'ciclana@email.com', 'ciclana00');
+const allUsers = (0, database_1.getAllUsers)();
+const createProductResult = (0, database_1.createProduct)("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.", "https://example.com/ssd-gamer.jpg");
+const allProducts = (0, database_1.getAllProducts)();
+const searchResults = (0, database_1.searchProductsByName)("gamer");
+console.log('Resultados da busca', searchResults);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import {users} from "./database"
-import { products } from "./database"
+import { users, products, createTUser, getAllUsers, createProduct, getAllProducts, searchProductsByName } from "./database";
 
-console.log('Usuários:', users);
-console.log('Produtos:', products);
+// Criando um novo usuário
+createTUser(
+  'u003',
+  'Ciclana',
+  'ciclana@email.com',
+  'ciclana00'
+);
+
+// Buscando todos os usuários
+const allUsers = getAllUsers();
+
+// Criando um novo produto
+const createProductResult = createProduct(
+    "prod003",
+    "SSD gamer",
+    349.99,
+    "Acelere seu sistema com velocidades incríveis de leitura e gravação.",
+    "https://example.com/ssd-gamer.jpg"
+  );
+  
+// Buscando todos os produtos
+const allProducts = getAllProducts();
+  
+const searchResults = searchProductsByName("gamer");
+console.log('Resultados da busca', searchResults)
